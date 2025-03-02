877dc745a1e4f9a49bffbbcfb5e72f65f31fe2b5
anothercoder-nik <nikunjagarwal449@gmail.com>
2025-03-02 18:08:16 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import './App.css'
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Values/Value";
+import Contacts from "./components/Contacts/Contacts";
+import GetStarted from "./components/GetStarted/GetStarted";
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
+      <Companies/>
+      <Residencies/>
+      <Value/>
+      <Contacts/>
+      <GetStarted/>
+    </div>
+  );
+}
+
+export default App;
